48b24d90ebb6bda7545870eef209c9c1e8727746
PedroJoaoRibeiro <pedro_ribeiro_93@hotmail.com>
2016-11-24 20:19:07 +0000

    Scatterplot update

--- javascript/scatterPlot.js
+++ javascript/scatterPlot.js
@@ -1,17 +1,115 @@
-var dataSet, scatterPlot;
-function main() {
-    dataSet = new Data("TOT");
-    //scatterPlot = new ScatterPlot();
+var dataSet, scatterPlot, barChart, average;
+
+//BUTTONS
+
+function doScatterPlot() {
+    var file = "data/Jogadores_VI/playoffs/2015.json";
+    d3.json(file, function (data) {
+        var aux = selectFromData(data, "Tm", "CLE");
+        scatterPlot = new ScatterPlot(aux);
+    });
+
 }
 
-function getPlayersDataByTeam(teamName, obj) {
-    for (i = 1950; i < 2016; i++) {
-        var file = "data/Jogadores_VI/playoffs/" + i + ".json";
-        d3.json(file, function (data) {
-            selectFromData(data, "Tm", teamName, obj);
-        });
+
+class ScatterPlot {
+    constructor(data) {
+        average = this.calculateAverage(data);
+        var w = 940,
+            h = 300,
+            pad = 20,
+            left_pad = 100;
+
+
+        var svg = d3.select("#the_scatterPlot")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        var tooltip = d3.select("#the_scatterPlot")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        var maxValue = d3.max(data.map(function (d) { return d.MP; }));
+        var x = d3.scaleLinear().domain([0, maxValue]).range([left_pad, w - pad]);
+        var y = d3.scaleLinear().domain([1.5, 0]).range([pad, h - pad * 2]);
+
+        var xAxis = d3.axisBottom().scale(x);
+        var yAxis = d3.axisLeft().scale(y);
+
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0, " + (h - pad) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
+            .call(yAxis);
+
+        svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("class", "circle")
+            .attr("cx", function (d) {
+                return x(d.MP);
+            })
+            .attr("cy", function (d) {
+                return y(d.Impact);
+            })
+            .attr("r", function (d) {
+                return 5;
+            })
+            .on("mouseover", function (d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html(d.Player.split("\\")[0] + "<br/>" + d.Impact.toFixed(2))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+        svg.append("line")
+            .style("stroke", "black")
+            .attr("x1", x(0))
+            .attr("x2", x(maxValue))
+            .attr("y1", y(average))
+            .attr("y2", y(average))
+            .on("mouseover", function (d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html("Impact " + average.toFixed(2))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    }
+
+    calculateAverage(data) {
+        var average = 0;
+        var cont = 0;
+        while (data.length > cont) {
+            average += data[cont].Impact;
+            cont++;
+        }
+        return average / cont;
     }
 }
+
+//Aux Methods
 
 function selectFromData(data, type, value) {
     var array = [];
@@ -21,76 +119,4 @@ function selectFromData(data, type, value) {
         }
     }
     return array;
-}
-
-class Data {
-    constructor(teamName) {
-        this.data = [];
-        getPlayersDataByTeam(teamName, this);
-    }
-    addData(array) {
-        this.data.push(array);
-    }
-    getData(i) {
-        return this.data[i];
-    }
-}
-
-
-class ScatterPlot {
-    constructor(aux) {
-        var w = 940,
-            h = 300,
-            pad = 20,
-            left_pad = 100;
-
-
-        this.svg = d3.select("#the_scatterPlot")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
-
-        var x = d3.scaleLinear().domain([2013, 2016]).range([left_pad, w - pad]);
-        var y = d3.scaleLinear().domain([1.5, 0]).range([pad, h - pad * 2]);
-
-        var xAxis = d3.axisBottom().scale(x);
-        var yAxis = d3.axisLeft().scale(y);
-
-
-        this.svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0, " + (h - pad) + ")")
-            .call(xAxis);
-
-        this.svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
-            .call(yAxis);
-
-        this.svg.selectAll("circle")
-            .data(aux)
-            .enter()
-            .append("circle")
-            .attr("class", "circle")
-            .attr("cx", function (d) {
-                return x(2015);
-            })
-            .attr("cy", function (d) {
-                return y(d.Impact);
-            })
-            .attr("r", function(d){
-                return 3;
-            });
-    }
-}
-
-//BUTTONS
-
-function doScatterPlot() {
-    var file = "data/Jogadores_VI/Players/2015.json";
-        d3.json(file, function (data) {
-            var aux = selectFromData(data, "Tm", "TOT");
-            scatterPlot = new ScatterPlot(aux);
-        });
-    
 }
